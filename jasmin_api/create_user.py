e052d201c3088325d4b513f6f0ba41f047e49a46
Lucas Otávio Teodoro Silva <lucas.teodoro@lotsystem.com.br>
2019-11-22 14:32:58 -0300

    create_user

--- jasmin_api/create_user.py
+++ jasmin_api/create_user.py
@@ -6,7 +6,10 @@ load_dotenv(find_dotenv())
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jasmin_api.settings")
 
-    from django.contrib.auth import get_user_model
+     from django.contrib.auth.management.commands.createsuperuser import get_user_model
 
-    User = get_user_model()
-    User.objects.create_superuser(os.getenv("USERNAME"), os.getenv("USER_EMAIL") or '',  os.getenv("USER_PASSWORD") or 'root')
+    get_user_model()._default_manager.db_manager('$DJANGO_DB_NAME').create_superuser(
+        username=os.getenv("USERNAME"),
+        email=os.getenv("USER_EMAIL") or '',
+        password=os.getenv("USER_PASSWORD") or 'root'
+    )
